66c6882636400a9d71f7efe7de82119fa4bef85b
이서현 <ishuyn31@gmail.com>
2026-07-09 13:08:40 +0900

    init

---
+++ scrapper.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+keyword = "python"
+url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}"
+r = requests.get(url)
+# print(r.text) # html코드를 들고옴
+
+soup = BeautifulSoup(r.text, "html.parser")
+result = soup.find_all("li", class_ = "c_col")
+print(len(result))
