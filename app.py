f1b9597a42d8d053014df20e4768607f5bbea852
이서현 <ishuyn31@gmail.com>
2026-07-09 16:19:46 +0900

    0709

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from scrapper import search_incruit
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/search')
+def search():
+    keyword = request.args.get('keyword')
+    print(keyword)
+    jobs = search_incruit(keyword)
+    print(jobs)
+    return render_template('search.html', jobs=enumerate(jobs))
+
+# @app.route("/hello")
+# def hello():
+#    return "안녕하세요"
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- scrapper.py
+++ scrapper.py
@@ -2,11 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 
 
-keyword = "python"
-url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}"
-r = requests.get(url)
-# print(r.text) # html코드를 들고옴
+def search_incruit(keyword):
 
-soup = BeautifulSoup(r.text, "html.parser")
-result = soup.find_all("li", class_ = "c_col")
-print(len(result))
+    url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}"
+    r = requests.get(url)
+    # print(r.text) # html코드를 들고옴
+
+    soup = BeautifulSoup(r.text, "html.parser")
+    lis = soup.find_all("li", class_ = "c_col")
+
+    jobs = []
+
+    for li in lis:
+        company = li.find("a", class_="cpname").text
+        title = li.find("div", class_="cell_mid").find("div", class_="cl_top").find("a").text
+        location = li.find("div", class_="cl_md").find_all("span")[0].text
+        link = li.find("div", class_="cell_mid").find("div", class_="cl_top").find("a").get("href")
+
+        job_data = {
+            "company": company,
+            "title": title,
+            "location": location,
+            "link": link
+        }
+
+        jobs.append(job_data)
+
+    return jobs
