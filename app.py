e648c75f0563301a189a3f12689e48ed9a6bd9e4
이서현 <ishuyn31@gmail.com>
2026-07-10 14:13:43 +0900

    0710 14:13

--- app.py
+++ app.py
@@ -9,38 +9,38 @@ page = 5
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/search')
+@app.route("/search")
 def search():
-    keyword = request.args.get('keyword')
-
-    if keyword == "":
-        return redirect("/")
-    
-    if keyword in db:
-        jobs = db[keyword]
-    else:
-        jobs = search_incruit(keyword, page)
-        db[keyword] = jobs
-
-    return render_template('search.html', jobs=enumerate(jobs), keyword=keyword, count=len(jobs))
-
-@app.route("/file")
-def file():
     keyword = request.args.get("keyword")
 
     if keyword == "":
         return redirect("/")
     
-    if keyword in db:
+    if keyword in db: 
         jobs = db[keyword]
-    
-    else:
+    else:    
         jobs = search_incruit(keyword, page)
+        db[keyword] = jobs
+    
+    return render_template("search.html", jobs=enumerate(jobs), keyword=keyword, count=len(jobs))
 
+@app.route("/file")
+def file():
+    keyword = request.args.get("keyword")
+
+    if keyword == "": 
+        return redirect("/")
+    
+    if keyword in db: 
+        jobs = db[keyword]
+    else:    
+        jobs = search_incruit(keyword, page)
+        
     save_to_csv(jobs)
     return send_file("./downloads.csv", as_attachment=True)
 
+    
 if __name__ == '__main__':
     app.run(debug=True)
