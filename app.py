a7dd30e877c72656727265a232f3eb38a8da373a
이서현 <ishuyn31@gmail.com>
2026-07-13 07:02:56 +0900

    111

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, send_file, redirect
-from scrapper import search_incruit
+from scrapper import search_incruit, search_saramin
 from file import save_to_csv
 
 app = Flask(__name__)
 
 db = {}
-page = 5
+page = 3
 
 @app.route('/')
 def hello_world():
@@ -21,7 +21,10 @@ def search():
     if keyword in db: 
         jobs = db[keyword]
     else:    
-        jobs = search_incruit(keyword, page)
+        incruit_jobs = search_incruit(keyword, page)
+        saramin_jobs =  search_saramin(keyword, page)
+
+        jobs = incruit_jobs + saramin_jobs
         db[keyword] = jobs
     
     return render_template("search.html", jobs=enumerate(jobs), keyword=keyword, count=len(jobs))
@@ -36,7 +39,10 @@ def file():
     if keyword in db: 
         jobs = db[keyword]
     else:    
-        jobs = search_incruit(keyword, page)
+        incruit_jobs = search_incruit(keyword, page)
+        saramin_jobs = search_saramin(keyword, page)
+        jobs = incruit_jobs + saramin_jobs
+        db[keyword] = jobs
         
     save_to_csv(jobs)
     return send_file("./downloads.csv", as_attachment=True)
--- scrapper.py
+++ scrapper.py
@@ -3,16 +3,15 @@ from bs4 import BeautifulSoup
 
 
 def search_incruit(keyword, page=1):
-
     jobs = []
 
     for i in range(page):
-        page = 30 * i
-        url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno={page}"
+        start_no = 30 * i
+        url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno={start_no}"
         r = requests.get(url)
-        # print(r.text) // html코드를 들고옴
         soup = BeautifulSoup(r.text, "html.parser")
-        lis = soup.find_all("li", class_ = "c_col")
+        
+        lis = soup.find_all("li", class_="c_col")[:30]
 
         for li in lis:
             company = li.find("a", class_="cpname").text
@@ -28,10 +27,59 @@ def search_incruit(keyword, page=1):
             }
 
             jobs.append(job_data)
-
+            
     return jobs
 
+
+def search_saramin(keyword, page=1):
+    jobs = []
+    
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+
+    for i in range(page):
+        page_num = i + 1
+        url = f"https://www.saramin.co.kr/zf_user/search/recruit?searchword={keyword}&recruitPage={page_num}"
+        
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.text, "html.parser")
+        
+        item_recruit = soup.find_all("div", class_="item_recruit")[:30]
+
+        for item in item_recruit:
+            company_div = item.find("div", class_="area_corp")
+            company = company_div.find("a").text.strip() if company_div else "회사명 없음"
+            
+            title_div = item.find("h2", class_="job_tit")
+            if title_div:
+                title_a = title_div.find("a")
+                title = title_a.text.strip()
+                link = "https://www.saramin.co.kr" + title_a.get("href")
+            else:
+                continue
+            
+            conditions_div = item.find("div", class_="job_condition")
+
+            if conditions_div:
+                location = conditions_div.find_all("span")[0].text.strip()
+            else:
+                location = "지역 정보 없음"
+
+            job_data = {
+                "company": company,
+                "title": title,
+                "location": location,
+                "link": link
+            }
+            jobs.append(job_data)
+            
+    return jobs
+
+
 if __name__ == '__main__':
-    result = search_incruit("간호사", 2)
-    print(result)
-    print(len(result))
+    
+    incruit_result = search_incruit("간호사", 2)
+    saramin_result = search_saramin("간호사", 2)
+    
+    total_result = incruit_result + saramin_result
